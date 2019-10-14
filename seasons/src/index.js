bc31c3758eb6d298f8f03cac14fb4efd5656d0a0
Ratna Lama <rlama@rlama.local>
2019-10-14 12:32:38 -0700

    added SeasonDisplay.css and Spinner.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  render() {
+  state = {lat: null, errorMessage: ''};
+
+  // best for data loading, API requests
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      
+      // callback 
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message}),
     );
-    return <div>Latitude: </div>;
-  }
+  } // end comopnentDidMount()
+
+  // componentDidUpdate() {
+  //   console.log('My component was updated')
+  // }
+
+  // componentWillUnmount() {}
+
+  // render() --> react requirement for class based component
+  render() {
+    // conditional rendering
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errormessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner />
+  } // end render() 
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
