5d74a1aced009cb94c26292c8f169e08034309fe
Ratna Lama <royalray7@gmail.com>
2022-01-19 21:40:22 -0800

    used custom useEffect hook to display fetch data from the API

--- react-hooks-demo/use-effect-hook/src/App.js
+++ react-hooks-demo/use-effect-hook/src/App.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
+
+import { useFetch } from './useFetch';
+
 import './App.css';
 
+// const url = 'http://numbersapi.com/43/trivia';
+
 function App() {
-  return <div>Hello World!</div>;
+  const [count, setCount] = useState(0);
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  return (
+    <div>
+      <div>{loading ? 'loading...' : 'FUN facts : ' + data}</div>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+    </div>
+  );
 }
 
 export default App;
