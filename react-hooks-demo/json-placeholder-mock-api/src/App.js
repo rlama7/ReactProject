663a681f187158826eeb617ac327224612704a15
Ratna Lama <royalray7@gmail.com>
2022-01-26 21:23:28 -0800

    initial setup for mock-api demo project

---
+++ react-hooks-demo/json-placeholder-mock-api/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div>Hello World!</div>
+      <div>JSON Placeholder Mock APi</div>
+    </div>
+  );
+};
