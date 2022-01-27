edf049af7457e0ee63ea2e60cc0bd0364d4a17b3
Ratna Lama <royalray7@gmail.com>
2022-01-26 20:25:18 -0800

    updated useEffect hook to store and retrieve data from localStorage

--- react-hooks-demo/use-effect-hook/src/App.js
+++ react-hooks-demo/use-effect-hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useFetch } from './useFetch';
 
@@ -7,13 +7,22 @@ import './App.css';
 // const url = 'http://numbersapi.com/43/trivia';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(JSON.parse(localStorage.getItem('count')));
+  // items will be stored as JSON strings, therefore will need to parse to convert to JavaScript object
   const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
+  // cache data to local storage
+  useEffect(() => {
+    localStorage.setItem('count', JSON.stringify(count));
+    // when saving data, it will be needed to convert JavaScript object to JSON strings.
+  }, [count]);
+
   return (
-    <div>
+    <div className="App">
       <div>{loading ? 'loading...' : 'FUN facts : ' + data}</div>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <div>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+      </div>
     </div>
   );
 }
