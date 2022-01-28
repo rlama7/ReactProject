ff621ad70ae2df3b29c9dacc95a1ba362455f709
Ratna Lama <royalray7@gmail.com>
2022-01-27 21:21:26 -0800

    add scss to react project

--- react-hooks-demo/json-placeholder-mock-api/src/App.js
+++ react-hooks-demo/json-placeholder-mock-api/src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
 import { useFetch } from './useFetch';
 
 export const App = () => {
@@ -15,7 +16,7 @@ export const App = () => {
         ) : (
           <div className="user-cards">
             {data.map((user) => (
-              <div key={user.phone}>
+              <div key={user.phone} className="users">
                 <h2>{user.name}</h2>
                 <h3>{user.email}</h3>
                 <p>{user.phone}</p>
