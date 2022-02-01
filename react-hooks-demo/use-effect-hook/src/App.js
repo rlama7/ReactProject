57dc0620560a4171f4c5264fa817f3a27aeb8832
Ratna Lama <royalray7@gmail.com>
2022-01-31 21:04:48 -0800

    added initial value for usestate

--- react-hooks-demo/use-effect-hook/src/App.js
+++ react-hooks-demo/use-effect-hook/src/App.js
@@ -7,7 +7,11 @@ import './App.css';
 // const url = 'http://numbersapi.com/43/trivia';
 
 function App() {
-  const [count, setCount] = useState(JSON.parse(localStorage.getItem('count')));
+  // get the initial value of count from localStorage
+  // only under initial render once.
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem('count'))
+  );
   // items will be stored as JSON strings, therefore will need to parse to convert to JavaScript object
   const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
