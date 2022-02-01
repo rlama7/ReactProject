15db9912f4a123d80676eb5cf090b426a60771a1
Ratna Lama <royalray7@gmail.com>
2022-01-31 21:55:13 -0800

    added useRef example

--- react-hooks-demo/use-ref-hook/src/App.js
+++ react-hooks-demo/use-ref-hook/src/App.js
@@ -1,11 +1,18 @@
+import React, { useRef } from 'react';
+
 import './App.css';
 
-function App() {
+export const App = () => {
+  const inputRef = useRef();
+
   return (
     <div className="App">
       <div>useRefHook Usage and eXamples</div>
+      <input ref={inputRef} name="email" />
+      {/** reference the html element in the node and can take any desired actions with useRef hook */}
+      <button onClick={() => console.log(inputRef.current.focus())}>
+        focus
+      </button>
     </div>
   );
-}
-
-export default App;
+};
